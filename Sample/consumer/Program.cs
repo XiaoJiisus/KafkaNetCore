176d5ced01ba0d923262b817c8689cd02cdbffc9
Ivan Vazquez <xiao-jiisus@outlook.com>
2023-04-05 23:46:13 -0600

    Sample kafka with purchases topic

--- Sample/consumer/Program.cs
+++ Sample/consumer/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+var config = new ConsumerConfig
+{
+    GroupId = "kafka-dotnet-getting-started",
+    BootstrapServers = "localhost:9092",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+const string topic = "purchases";
+
+CancellationTokenSource cts = new CancellationTokenSource();
+Console.CancelKeyPress += (_, e) => {
+    e.Cancel = true; // prevent the process from terminating.
+    cts.Cancel();
+};
+
+using (var consumer = new ConsumerBuilder<string, string>(config).Build())
+{
+    consumer.Subscribe(topic);
+    
+    try
+    {
+        while (true)
+        {
+            var cr = consumer.Consume(cts.Token);
+            Console.WriteLine($"Consumed event from topic {topic} with key {cr.Message.Key,-10} and value {cr.Message.Value}");
+        }
+    }
+    catch (OperationCanceledException)
+    {
+        // Ctrl-C was pressed.
+    }
+    finally
+    {
+        consumer.Close();
+    }
+}
--- Sample/producer/Program.cs
+++ Sample/producer/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+using System;
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092"};
+
+const string topic = "purchases";
+
+string[] users = { "eabara", "jsmith", "sgarcia", "jbernard", "htanaka", "awalther" };
+string[] items = { "book", "alarm clock", "t-shirts", "gift card", "batteries" };
+
+using (var producer = new ProducerBuilder<string, string>(config).Build())
+{
+    var numProduced = 0;
+    Random rnd = new Random();
+    const int numMessages = 10;
+    
+    for (int i = 0; i < numMessages; ++i)
+    {
+        var user = users[rnd.Next(users.Length)];
+        var item = items[rnd.Next(items.Length)];
+
+        producer.Produce(topic, new Message<string, string> { Key = user, Value = item },
+            (deliveryReport) =>
+            {
+                if (deliveryReport.Error.Code != ErrorCode.NoError)
+                {
+                    Console.WriteLine($"Failed to deliver message: {deliveryReport.Error.Reason}");
+                }
+                else
+                {
+                    Console.WriteLine($"Produced event to topic {topic}: key = {user,-10} value = {item}");
+                    numProduced += 1;
+                }
+            });
+    }
+
+    producer.Flush(TimeSpan.FromSeconds(10));
+    Console.WriteLine($"{numProduced} messages were produced to topic {topic}");
+}
